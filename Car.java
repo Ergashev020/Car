c2779b3d4dfdb7c2a63064bbba903188197e1979
Ergashev020 <shukurulla020ergashev@gmail.com>
2024-08-27 08:39:39 +0500

    Class_Car
    Class_Car

---
+++ Car.java
@@ -0,0 +1,43 @@
+package AmalivVazifalar;
+
+public class Car {
+    public  void getColor(String color) {
+        System.out.println(color);
+    }
+    public void getmodel(String model){
+        System.out.println(model);
+    }
+    public void getprice(String price){
+        System.out.println(price);
+    }
+    public void getsystem(String system){
+        System.out.println(system);
+    }
+}
+class triangle{
+    public void geta(int a){
+        System.out.println(a);
+    }
+    public void getb(int b){
+        System.out.println(b);
+    }
+    public void get(int c){
+        System.out.println(c);
+    }
+
+}
+class person {
+    public void name(String name){
+        System.out.println(name);
+    }
+    public void surname(String surname){
+        System.out.println(surname);
+    }
+    public void age(int age){
+        System.out.println(age);
+    }
+    public void year(int year){
+        System.out.println(year);
+    }
+}
+
